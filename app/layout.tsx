214dcd53d1782f4de6c10ee7b1c2678cfb9faa40
Yevhenii <jenmikitenko@gmail.com>
2024-01-19 16:18:47 +0200

    fix problems casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import "@mantine/core/styles.css";
 import '@mantine/dates/styles.css';
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { MantineProvider } from "@mantine/core";
 
