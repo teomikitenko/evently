b90f96741a9ecdf79dac3471bb18eaa30246527b
Yevhenii <jenmikitenko@gmail.com>
2024-01-19 18:52:32 +0200

    fix images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
           {
             protocol: 'https',
-            hostname: 'vthbjyvxqzqwhycurblq.supabase.co',
+            hostname: /* 'vthbjyvxqzqwhycurblq.supabase.co' */"**.supabase.co",
           },
         ],
       },
