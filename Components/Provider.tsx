aa9996d03b676160e5bf307f36688dda872f0b59
Yevhenii <jenmikitenko@gmail.com>
2024-01-19 17:32:41 +0200

    fix2

--- Components/Provider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import React from "react";
-import { MantineProvider } from "@mantine/core";
-
-const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <MantineProvider>{children}</MantineProvider>;
-};
-
-export default Provider;
