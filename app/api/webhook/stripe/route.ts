f22820aeaa94462e0e5b1aa936e90c86bcbfd20c
Yevhenii <jenmikitenko@gmail.com>
2024-01-20 15:46:36 +0200

    fix stripe

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,12 +1,13 @@
-const stripe = require('stripe')('sk_test_...');
+import stripe from 'stripe'
+import { NextResponse } from 'next/server'
 
 export async function POST(request:Request){
-  const sig = await request.text()
-
+  const body = await request.text()
   let event;
-
+  const sig = request.headers.get('stripe-signature') as string
+  const endpointSecret = process.env.NEXT_PUBLIC_WEBHOOK_SECRET as string
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, process.env.NEXT_PUBLIC_WEBHOOK_SECRET!);
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
     return new Response(`Webhook error: ${err!}`, {
         status: 400,
@@ -17,9 +18,8 @@ export async function POST(request:Request){
    switch (event.type) {
     case 'checkout.session.completed':
       const checkoutSessionCompleted = event.data.object;
-      console.log(`Checkout session completed ${checkoutSessionCompleted}`);
       // Then define and call a function to handle the event checkout.session.completed
-      break;
+      return NextResponse.json({ message: 'OK', data:checkoutSessionCompleted})
     // ... handle other event types
      default:
       console.log(`Unhandled event type ${event.type}`); 
