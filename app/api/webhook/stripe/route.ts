eb3fce8aae6ea304956bf68c2e9d054e5e53da8d
Yevhenii <jenmikitenko@gmail.com>
2024-01-20 14:12:10 +0200

    add route test

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,11 +1,35 @@
-const stripe = require('stripe')(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!);
-const endpointSecret = "whsec_77ba44543863e0c12b5515baff68c8a4c35df3b6269f5bd2ddd95a15b49731ea";
+export const dynamic = 'force-dynamic'
+const stripe = require('stripe')('sk_test_...');
 
+export async function POST(request:Request){
+  const sig = await request.text()
 
-export async function POST(request:Request) {
-    const sig = request.headers.get('stripe-signature')
-    console.log(sig)
-    return new Response('Success!', {
-        status: 200,
-      })
-}
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, process.env.NEXT_PUBLIC_WEBHOOK_SECRET!);
+  } catch (err) {
+    return new Response(`Webhook error: ${err!}`, {
+        status: 400,
+      })    
+  }
+
+   // Handle the event
+   switch (event.type) {
+    case 'checkout.session.completed':
+      const checkoutSessionCompleted = event.data.object;
+      console.log(`Checkout session completed ${checkoutSessionCompleted}`);
+      // Then define and call a function to handle the event checkout.session.completed
+      break;
+    // ... handle other event types
+     default:
+      console.log(`Unhandled event type ${event.type}`); 
+  }
+
+  // Handle the event
+/*   console.log(`Unhandled event type ${event.type}`); */
+  return new Response('Success!', {
+    status: 200,
+  })
+  
+};
