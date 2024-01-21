9008024bd781fcc43af8a66246576b1db9524572
Yevhenii <jenmikitenko@gmail.com>
2024-01-21 21:23:58 +0200

    add table order

--- components/TableOrders.tsx
+++ components/TableOrders.tsx
@@ -1,37 +1,52 @@
-'use client'
-import { Table } from '@mantine/core';
-import type { Orders } from '@/configs/types/types'
-const TableOrders = ({orders}:{orders:Orders}) => {
-  const tableHeaders = ['Order Id','Event Title','Buyer','Created','Amount']
-  const bodyRows = orders.map(o=>{
-    const currentPrice = o.events?.free ? 'Free':o.events?.price;
-    return(
-      <Table.Tr key={o.id} >
-      <Table.Td></Table.Td>
-      <Table.Td><p className='line-clamp-1'>{o.events?.title}</p></Table.Td>
-      <Table.Td><p>{o.name}</p></Table.Td>
-      <Table.Td><p>{o.created_at}</p> </Table.Td>
-      <Table.Td><p>{currentPrice}</p> </Table.Td>
-    </Table.Tr>
-    )
-    }
-  )
-  return (
-    <Table horizontalSpacing={'lg'}>
-    <Table.Thead>
-      <Table.Tr>
-        {tableHeaders.map(e=><Table.Th key={e}>{e}</Table.Th>)}
+"use client";
+import { Table } from "@mantine/core";
+import type { Orders } from "@/configs/types/types";
+const TableOrders = ({ orders }: { orders: Orders }) => {
+  const tableHeaders = [
+    "Order Id",
+    "Event Title",
+    "Buyer",
+    "Created",
+    "Amount",
+  ];
+  const bodyRows = orders.map((o) => {
+    const currentPrice = o.events?.free ? "Free" : o.events?.price;
+    return (
+      <Table.Tr className="w-full" key={o.id}>
+        <Table.Td className="w-[calc(100%/5)]">
+          <p className="line-clamp-1 break-words">{o.order_id}</p>
+        </Table.Td>
+        <Table.Td className="w-[calc(100%/5)]">
+          <p className="line-clamp-1 break-words">{o.events?.title}</p>
+        </Table.Td>
+        <Table.Td className="w-[calc(100%/5)]">
+          <p className="line-clamp-1 break-words">{o.name}</p>
+        </Table.Td>
+        <Table.Td className="w-[calc(100%/5)]">
+          <p className="line-clamp-1 break-words">{o.created_at.split("T")[0]}</p>
+        </Table.Td>
+        <Table.Td className="w-[calc(100%/5)]">
+          <p className="line-clamp-1 break-words">{currentPrice}</p>
+        </Table.Td>
       </Table.Tr>
-    </Table.Thead>
-    <Table.Tbody>{bodyRows}</Table.Tbody>
-  </Table>
-  )
-}
+    );
+  });
+  return (
+    <Table horizontalSpacing={"xl"} layout="fixed">
+      <Table.Thead>
+        <Table.Tr>
+          {tableHeaders.map((e) => (
+            <Table.Th key={e}>{e}</Table.Th>
+          ))}
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{bodyRows}</Table.Tbody>
+    </Table>
+  );
+};
 
-export default TableOrders
+export default TableOrders;
 
-const NotFoundDetails=()=>{
-  return(
-    <p>not found</p>
-  )
-}
+const NotFoundDetails = () => {
+  return <p>not found</p>;
+};
